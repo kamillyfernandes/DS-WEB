f93a9590c81cc94a4e62a730583c53e0391c2a9a
kamillyfernandes <kamillyfer19@gmail.com>
2025-01-31 12:25:13 -0300

    Pirmeiro commit

---
+++ Javascript/Aula-01/readme.md
@@ -0,0 +1,7 @@
+aula introdutora sobre Lógica de Programação com Javascript
+
+Declaração de variaveis 
+comandos = alert,confirm,prompt, adiçao
+
+
+
---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,23 @@
+console.log("Ola  mundo");
+
+//Exemplo alert()
+    alert("Ola mundo 1")
+
+//Exemplo de confirm()
+    var teste = confirm("Ola mundo 2") //mostrar se a resposta e sim ou cancelar
+    //exibindo a opcao escolhida pelo usuário
+    console.log(teste)
+
+//Exemplo de Prompt
+    var testePrompt = prompt("Digite seu nome")
+    //exibindo o nome do usuário
+    console.log(testePrompt)
+    alert(testePrompt)
+
+//Tentativa adicao
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero"))
+    //Verificando o tipo da variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
+
